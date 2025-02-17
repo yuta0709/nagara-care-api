fd0ef0ef96a71938dbd76ba72c9fd705a89d9894
yuta0709 <58354812+yuta0709@users.noreply.github.com>
2025-02-18 01:26:28 +0900

    feat: openapi.yamlの出力を追加

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as yaml from 'yaml';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-
+async function writeOpenAPISpec(app: any) {
   const config = new DocumentBuilder()
     .setTitle('Nagara Care API')
     .setVersion('1.0')
@@ -22,8 +21,18 @@ async function bootstrap() {
       'JWT-auth',
     )
     .build();
-  const documentFactory = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, documentFactory);
+  const document = SwaggerModule.createDocument(app, config);
+  const yamlString = yaml.stringify(document);
+  fs.writeFileSync('./openapi.yaml', yamlString);
+  return document;
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const document = await writeOpenAPISpec(app);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
