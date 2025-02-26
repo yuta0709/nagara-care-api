9c6b77c9e49d0de16977628ef6d5c61a64065fa3
yuta0709 <58354812+yuta0709@users.noreply.github.com>
2025-02-26 13:23:07 +0900

    Prismaのページネーションライブラリを追加

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,9 +1,20 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { paginate } from 'prisma-extension-pagination';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+  }
+
+  pg() {
+    return this.$extends({
+      model: {
+        $allModels: {
+          paginate,
+        },
+      },
+    });
   }
 }
