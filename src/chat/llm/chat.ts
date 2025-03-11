47284f990a17d5a976a6e4e39f74969620408ed8
yuta0709 <58354812+yuta0709@users.noreply.github.com>
2025-03-12 02:05:58 +0900

    wip

--- src/chat/llm/chat.ts
+++ src/chat/llm/chat.ts
@@ -3,6 +3,11 @@ import { ChatOpenAI } from '@langchain/openai';
 
 export async function getAiResponse(messages: ChatMessage[]) {
   const model = new ChatOpenAI({ model: 'gpt-4o-mini' });
-  const response = await model.invoke(messages);
+  const systemMessage = new ChatMessage({
+    role: 'system',
+    content:
+      'あなたは介護施設に特化したRAGモデルです。[Context]には介護施設のデータベースから取得したデータが入っています。[User]には利用者の質問が入っています。[User]の質問に対して、[Context]を参照して回答してください。',
+  });
+  const response = await model.invoke([systemMessage, ...messages]);
   return response.content;
 }
